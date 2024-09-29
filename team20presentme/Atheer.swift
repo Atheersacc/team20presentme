664ca3cf502a5d4753b58c958affa8c327f21eab
Atheer <Atheertalmutairi@gmail.com>
2024-09-29 16:05:49 +0300

    PHOTO

---
+++ team20presentme/Atheer.swift
@@ -0,0 +1,26 @@
+//
+//  Atheer.swift
+//  team20presentme
+//
+//  Created by Atheer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Atheer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(.atheeer)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 400,
+                   height: 400)
+            .cornerRadius(400)
+        
+            
+    }
+}
+
+#Preview {
+    Atheer()
+}
---
+++ team20presentme/Jood.swift
@@ -0,0 +1,18 @@
+//
+//  Jood.swift
+//  team20presentme
+//
+//  Created by Atheer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Jood: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Jood()
+}
