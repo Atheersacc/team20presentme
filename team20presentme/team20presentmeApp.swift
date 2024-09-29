47bc8dd860752b94bb419811083fdba84a3455f3
Atheer <Atheertalmutairi@gmail.com>
2024-09-29 15:23:18 +0300

    Initial Commit

---
+++ team20presentme/team20presentmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  team20presentmeApp.swift
+//  team20presentme
+//
+//  Created by Atheer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct team20presentmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
