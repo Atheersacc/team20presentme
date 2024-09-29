9977a3ba7791ca89c21f31721f75a0edc02e555b
Atheer <Atheertalmutairi@gmail.com>
2024-09-29 15:41:18 +0300

    add team

---
+++ team20presentme/Njoud.swift
@@ -0,0 +1,18 @@
+//
+//  Njoud.swift
+//  team20presentme
+//
+//  Created by Atheer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct Njoud: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Njoud()
+}
---
+++ team20presentme/teef.swift
@@ -0,0 +1,18 @@
+//
+//  teef.swift
+//  team20presentme
+//
+//  Created by Atheer on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+struct teef: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    teef()
+}
